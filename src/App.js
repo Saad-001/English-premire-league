3d54eac37c64943f5a567915ecd73a71096a8c13
Saad-001 <saadafnan21@gmail.com>
2021-03-11 12:51:49 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Home from './Components/Home/Home';
 import TeamDetails from './Components/TeamDetails/TeamDetails';
 import NotFound from './Components/NotFound/NotFound';
